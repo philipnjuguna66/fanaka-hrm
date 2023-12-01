b5811ac0caf84e8d11fcc5987782e55c75202815
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-01 13:34:32 +0300

    wip: work deductions and benefits on payroll

--- app/Imports/EmployeeImport.php
+++ app/Imports/EmployeeImport.php
@@ -6,6 +6,7 @@ use App\Models\Employee;
 use App\Models\JobTitle;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
@@ -20,53 +21,62 @@ class EmployeeImport implements ToCollection, WithHeadingRow
     {
         $collection->each(function ($data){
 
+            try {
+                DB::beginTransaction();
+                if (! Employee::query()->where('legal_document_number', $data['id_no'])->exists()){
+
+                    if (isset($data['full_name']))
+                    {
+                        $full_name = explode('_', str($data['full_name'])->slug('_')->value() );
+
+                        $data['first_name'] = $full_name[0] ?? null;
+                        $data['middle_name'] = $full_name[1] ?? null;
+                        $data['last_name'] = $full_name[2] ?? null;
+                    }
+
+                    tap(Employee::query()->where([ 'legal_document_number' => $data['id_no']])->firstOrCreate([
+                        'first_name' => $data['first_name'],
+                        'middle_name' => $data['middle_name'],
+                        'last_name' => $data['last_name'],
+                        'gender' => $data['gender'],
+                        'date_of_birth' => Carbon::parse(Date::excelToDateTimeObject($data['date_of_birth'])),
+                        'legal_document_type' => 'nat',
+                        'legal_document_number' => $data['id_no'],
+                        'kra_pin_no' => $data['kra_pin_no'] ?? null,
+                        'nssf_no' => $data['nssf_no'] ?? 0,
+                        'nhif_no' => $data['nhif_no'] ?? 0,
+                    ]), function (Employee $employee) use ($data){
+                        $employee->salaryDetail()->create([
+                            'basic_salary' => $data['basic_salary'] ?? 0
+                        ]);
+
+                        $employee->hrDetail()->create([
+                            'staff_number' => $data['staff_no'] ?? $employee->id,
+                            'job_title_id' => JobTitle::query()->where('name', "like", "%{$data['job_title']}%")
+                                ->firstOrCreate([
+                                    'label' => $data['job_title']
+                                ]),
+
+                            'date_of_employment' => Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])),
+                            'contract_start' => Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])),
+                        ]);
+
+                        $employee->hrContact()->create([
+                            'office_phone_number' => $data['phone_number'] ?? 0
+                        ]);
 
 
-           if (! Employee::query()->where('legal_document_number', $data['id_no'])->exists()){
+                    });
+                }
 
-               if (isset($data['full_name']))
-               {
-                   $full_name = explode('_', str($data['full_name'])->slug('_')->value() );
-
-                   $data['first_name'] = $full_name[0] ?? null;
-                   $data['middle_name'] = $full_name[1] ?? null;
-                   $data['last_name'] = $full_name[2] ?? null;
-               }
-
-               tap(Employee::query()->where([ 'legal_document_number' => $data['id_no']])->firstOrCreate([
-                   'first_name' => $data['first_name'],
-                   'middle_name' => $data['middle_name'],
-                   'last_name' => $data['last_name'],
-                   'gender' => $data['gender'],
-                   'date_of_birth' => Carbon::parse(Date::excelToDateTimeObject($data['date_of_birth'])),
-                   'legal_document_type' => 'nat',
-                   'legal_document_number' => $data['id_no'],
-                   'kra_pin_no' => $data['kra_pin_no'] ?? null,
-                   'nssf_no' => $data['nssf_no'] ?? 0,
-                   'nhif_no' => $data['nhif_no'] ?? 0,
-               ]), function (Employee $employee) use ($data){
-                   $employee->salaryDetail()->create([
-                       'basic_salary' => $data['basic_salary'] ?? 0
-                   ]);
-
-                   $employee->hrDetail()->create([
-                       'staff_number' => $data['staff_no'] ?? $employee->id,
-                       'job_title_id' => JobTitle::query()->where('name', "like", "%{$data['job_title']}%")
-                       ->firstOrCreate([
-                           'label' => $data['job_title']
-                       ]),
-
-                       'date_of_employment' => Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])),
-                       'contract_start' => Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])),
-                   ]);
-
-                   $employee->hrContact()->create([
-                       'office_phone_number' => $data['phone_number'] ?? 0
-                   ]);
+                DB::commit();
+            }
+            catch (\Exception $e)
+            {
+                DB::rollBack();
+            }
 
 
-               });
-           }
         });
     }
 
