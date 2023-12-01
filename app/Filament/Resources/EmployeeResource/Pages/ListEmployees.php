5d3f3dd2a53f25f8aea1faa12515f23cb86c9920
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-01 12:49:50 +0300

    wip: work deductions and benefits on payroll

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -37,9 +37,7 @@ class ListEmployees extends ListRecords
             ->action(function (array $data){
                 try {
 
-                    dd($data['employee_file']);
-
-                    Excel::import(new EmployeeImport(),  $data['employee_file']);
+                    Excel::import(new EmployeeImport, ($data['employee_file']));
 
                     return Notification::make('error')
                         ->success()
