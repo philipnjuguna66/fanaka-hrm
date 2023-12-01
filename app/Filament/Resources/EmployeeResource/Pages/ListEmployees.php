b48b15bbed6811bb54da8558faf64cdc21e4c263
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-01 14:35:34 +0300

    wip: work deductions and benefits on payroll

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -4,17 +4,25 @@ namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use App\Filament\Resources\EmployeeResource;
 use App\Imports\EmployeeImport;
+use App\Models\Employee;
+use App\Models\JobTitle;
+use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Set;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Support\HtmlString;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 use Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class ListEmployees extends ListRecords
 {
     protected static string $resource = EmployeeResource::class;
 
+    public $employee_file;
     protected function getHeaderActions(): array
     {
         return [
@@ -25,32 +33,62 @@ class ListEmployees extends ListRecords
 
     private function importEmployees() : Actions\Action
     {
-        return  Actions\Action::make('import Employees')
-            ->form([
-                FileUpload::make('employee_file')
-                ->required()
+        return  ImportAction::make()
+            ->fields([
+                ImportField::make('employee_file')
+                    ->label('Employee List')
+                    ->helperText('Define as employee list'),
             ])
             ->slideOver()
             ->closeModalByClickingAway()
             ->modalHeading("Upload excel with employee data")
             ->modalContent(fn()  => new HtmlString("<a href='". asset('templates/employee_template.xlsx')."'>Download Template</a>") )
-            ->action(function (array $data){
-                try {
+            ->handleRecordCreation(function(array $data) {
+                dd($data);
+                if (! Employee::query()->where('legal_document_number', $data['id_no'])->exists()){
 
-                    Excel::import(new EmployeeImport, ($data['employee_file']));
+                    if (isset($data['full_name']))
+                    {
+                        $full_name = explode('_', str($data['full_name'])->slug('_')->value() );
 
-                    return Notification::make('error')
-                        ->success()
-                        ->title("Uploading")
-                        ->send();
-                }
-                catch (\Exception $exception)
-                {
-                    return Notification::make('error')
-                        ->danger()
-                        ->title("Something went wrong")
-                        ->body($exception->getMessage())
-                        ->send();
+                        $data['first_name'] = $full_name[0] ?? null;
+                        $data['middle_name'] = $full_name[1] ?? null;
+                        $data['last_name'] = $full_name[2] ?? null;
+                    }
+
+                    tap(Employee::query()->where([ 'legal_document_number' => $data['id_no']])->firstOrCreate([
+                        'first_name' => $data['first_name'],
+                        'middle_name' => $data['middle_name'],
+                        'last_name' => $data['last_name'],
+                        'gender' => $data['gender'],
+                        'date_of_birth' => isset($data['date_of_birth']) ? Carbon::parse(Date::excelToDateTimeObject($data['date_of_birth'])) : null,
+                        'legal_document_type' => 'nat',
+                        'legal_document_number' => $data['id_no'],
+                        'kra_pin_no' => $data['kra_pin_no'] ?? null,
+                        'nssf_no' => $data['nssf_no'] ?? 0,
+                        'nhif_no' => $data['nhif_no'] ?? 0,
+                    ]), function (Employee $employee) use ($data){
+                        $employee->salaryDetail()->create([
+                            'basic_salary' => $data['basic_salary'] ?? 0
+                        ]);
+
+                        $employee->hrDetail()->create([
+                            'staff_number' => $data['staff_no'] ?? $employee->id,
+                            'job_title_id' => JobTitle::query()->where('name', "like", "%{$data['job_title']}%")
+                                ->firstOrCreate([
+                                    'label' => $data['job_title']
+                                ]),
+
+                            'date_of_employment' => isset($data['date_joining']) ? Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])) : null,
+                            'contract_start' => Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])),
+                        ]);
+
+                        $employee->hrContact()->create([
+                            'office_phone_number' => $data['phone_number'] ?? 0
+                        ]);
+
+
+                    });
                 }
             });
 
--- app/Imports/EmployeeImport.php
+++ app/Imports/EmployeeImport.php
@@ -12,63 +12,6 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class EmployeeImport implements ToCollection, WithHeadingRow
+class EmployeeImport
 {
-    /**
-    * @param Collection $collection
-    */
-    public function collection(Collection $collection)
-    {
-        $collection->each(function ($data){
-
-
-                if (! Employee::query()->where('legal_document_number', $data['id_no'])->exists()){
-
-                    if (isset($data['full_name']))
-                    {
-                        $full_name = explode('_', str($data['full_name'])->slug('_')->value() );
-
-                        $data['first_name'] = $full_name[0] ?? null;
-                        $data['middle_name'] = $full_name[1] ?? null;
-                        $data['last_name'] = $full_name[2] ?? null;
-                    }
-
-                    tap(Employee::query()->where([ 'legal_document_number' => $data['id_no']])->firstOrCreate([
-                        'first_name' => $data['first_name'],
-                        'middle_name' => $data['middle_name'],
-                        'last_name' => $data['last_name'],
-                        'gender' => $data['gender'],
-                        'date_of_birth' => isset($data['date_of_birth']) ? Carbon::parse(Date::excelToDateTimeObject($data['date_of_birth'])) : null,
-                        'legal_document_type' => 'nat',
-                        'legal_document_number' => $data['id_no'],
-                        'kra_pin_no' => $data['kra_pin_no'] ?? null,
-                        'nssf_no' => $data['nssf_no'] ?? 0,
-                        'nhif_no' => $data['nhif_no'] ?? 0,
-                    ]), function (Employee $employee) use ($data){
-                        $employee->salaryDetail()->create([
-                            'basic_salary' => $data['basic_salary'] ?? 0
-                        ]);
-
-                        $employee->hrDetail()->create([
-                            'staff_number' => $data['staff_no'] ?? $employee->id,
-                            'job_title_id' => JobTitle::query()->where('name', "like", "%{$data['job_title']}%")
-                                ->firstOrCreate([
-                                    'label' => $data['job_title']
-                                ]),
-
-                            'date_of_employment' => isset($data['date_joining']) ? Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])) : null,
-                            'contract_start' => Carbon::parse(Date::excelToDateTimeObject($data['date_joining'])),
-                        ]);
-
-                        $employee->hrContact()->create([
-                            'office_phone_number' => $data['phone_number'] ?? 0
-                        ]);
-
-
-                    });
-                }
-
-        });
-    }
-
 }
