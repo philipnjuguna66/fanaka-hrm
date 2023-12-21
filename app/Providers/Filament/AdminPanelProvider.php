fc072e470167414b5340977f1e7745a6f7fdac73
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-21 11:09:32 +0300

    wip: work deductions and benefits on payroll

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,8 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->maxContentWidth('full')
+            ->collapsedSidebarWidth('250px')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
