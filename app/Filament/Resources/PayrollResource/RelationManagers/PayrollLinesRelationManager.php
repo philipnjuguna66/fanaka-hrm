963eb11ed40ac55af7db4d61469656674bdda523
Njuguna Philip <philipnjuguna66@gmail.com>
2024-02-01 15:51:19 +0300

    employee bebfits

--- app/Filament/Resources/PayrollResource/RelationManagers/PayrollLinesRelationManager.php
+++ app/Filament/Resources/PayrollResource/RelationManagers/PayrollLinesRelationManager.php
@@ -31,49 +31,8 @@ class PayrollLinesRelationManager extends RelationManager
 
     public function table(Table $table): Table
     {
-        $deductions = Deduction::query()
-            ->get();
-
-        $benefits = Benefit::query()
-            ->get();
-        $statutoryDeductions = StatutoryDeduction::query()
-            ->get();
-
-        $deductionColumns = [];
-
-        $statutoryDeductionColumns = [];
-
-        foreach ($deductions as $deduction) {
-
-            $deductionColumns[] = TextColumn::make($deduction->name)->searchable()->numeric(2)
-                ->toggleable(isToggledHiddenByDefault: true);
-
-        }
-        $benefitColumns = [];
-
-        foreach ($benefits as $benefit) {
-
-            $benefitColumns[] = TextColumn::make($benefit->name)->searchable()->numeric(2)
-                ->toggleable(isToggledHiddenByDefault: true);
-
-        }
 
 
-        $benefitColumns = [];
-
-        foreach ($benefits as $benefit) {
-
-            $benefitColumns[] = TextColumn::make($benefit->name)->searchable()->numeric(2)
-                ->toggleable(isToggledHiddenByDefault: true);
-
-        }
-
-        foreach ($statutoryDeductions as $statutoryDeduction) {
-
-            $statutoryDeductionColumns[] = TextColumn::make($statutoryDeduction->name)->searchable()->numeric(2)
-                ->toggleable(isToggledHiddenByDefault: true);
-
-        }
 
         return $table
             ->recordTitleAttribute('employee.first_name')
@@ -102,9 +61,6 @@ class PayrollLinesRelationManager extends RelationManager
                 Tables\Columns\TextColumn::make('insurance_relief')
                     ->numeric()
                     ->sortable(),
-                ...$deductionColumns,
-                ...$benefitColumns,
-                ...$statutoryDeductionColumns,
                 Tables\Columns\TextColumn::make('net_payee')
                     ->numeric()
                     ->sortable(),
