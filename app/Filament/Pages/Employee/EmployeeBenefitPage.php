0c32db85c737a554f8ac513c98129e43ef35582b
Njuguna Philip <philipnjuguna66@gmail.com>
2024-02-01 10:16:43 +0300

    employee bebfits

--- app/Filament/Pages/Employee/EmployeeBenefitPage.php
+++ app/Filament/Pages/Employee/EmployeeBenefitPage.php
@@ -39,7 +39,7 @@ class EmployeeBenefitPage extends Page implements HasTable
         return  $table->query(EmployeeBenefit::query())
             ->columns([
                 TextColumn::make('employee.name')->searchable(),
-                TextColumn::make('benefit.name')->getStateUsing(fn(EmployeeBenefit $record) => $record->benefit),
+                TextColumn::make('benefit_id'),
                 TextColumn::make('amount')->numeric(),
             ])
             ->filters([
