3e9ba01f2ce92564b4f434b4aceb17eac4186ffd
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-06-21 16:12:52 +0300

    requiresConfirmation

--- app/Filament/Pages/Employee/EmployeeBenefitPage.php
+++ app/Filament/Pages/Employee/EmployeeBenefitPage.php
@@ -59,7 +59,12 @@ class EmployeeBenefitPage extends Page implements HasTable
                 TextColumn::make('employee.first_name')
                     ->getStateUsing(fn(EmployeeBenefit $record) => $record->employee->name)->searchable(),
                 TextColumn::make('benefit.name'),
-                TextInputColumn::make('amount'),
+                TextInputColumn::make('amount')
+                ->beforeStateUpdated(function ($record, $state, $action) {
+
+                    return $action->requiresConfirmation();
+                    // Runs before the state is saved to the database.
+                }),
             ])
             ->filters([
                 SelectFilter::make('Benefit')
